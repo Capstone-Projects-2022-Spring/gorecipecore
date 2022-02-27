dea6d7de335f503aa00d9e54dcb2cb9dcb7bae0a
ylebovits <yakirlebovits@temple.edu>
2022-02-27 07:23:56 -0500

    trying to fix workflow tests

--- src/test/java/com/cis/gorecipe/controller/UserControllerTest.java
+++ src/test/java/com/cis/gorecipe/controller/UserControllerTest.java
@@ -123,6 +123,7 @@ class UserControllerTest {
         User actual = UserDTO.mapToUser(serializer.readValue(result, UserDTO.class));
         Optional<User> storedUser = userRepository.findById(actual.getId());
         assertTrue(storedUser.isPresent());
+        logger.debug(Boolean.toString(storedUser.get().equals(actual)));
         assertEquals(storedUser.get(), actual);
     }
 
