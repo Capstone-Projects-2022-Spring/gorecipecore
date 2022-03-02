4f4ab1feb1d8bb565996caedea00da3427787b34
ylebovits <yakirlebovits@temple.edu>
2022-03-02 14:28:39 -0500

    trying to fix GH actions testing

--- src/main/java/com/cis/gorecipe/model/User.java
+++ src/main/java/com/cis/gorecipe/model/User.java
@@ -105,7 +105,10 @@ public class User {
     public boolean equals(Object o) {
 
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) {
+            System.out.println("here!");
+            return false;
+        }
         User user = (User) o;
 
         return id.equals(user.id) &&
